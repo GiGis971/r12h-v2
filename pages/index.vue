7e09969b7117e724ed0fdcb04426b1491d940fac
regis <regis@bird-office.com>
2020-05-17 05:46:03 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <logo />
+        <h1 class="title">
+          r12h
+        </h1>
+        <h2 class="subtitle">
+          My tiptop Nuxt.js project
+        </h2>
+        <div class="links">
+          <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+            Documentation
+          </a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      key: 'value'
+    }
+  }
+}
+</script>
+
+<style></style>
